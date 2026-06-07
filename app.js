d85c8b195faa558a17b6b3ca188706d9d55f839e
Mega-Tekhnika <zesuprillke@mail.ru>
2026-06-07 16:50:34 +0400

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
-function login(){
+function login() {
 
-    const order =
-        document.getElementById("orderNumber").value;
+```
+const order = document.getElementById("orderNumber").value.trim();
+const code = document.getElementById("accessCode").value.trim();
 
-    const code =
-        document.getElementById("accessCode").value;
+if (order === "123456" && code === "123456") {
 
-    if(order === "9203" && code === "MTK-9203"){
+    window.location.href = "cabinet.html";
 
-        window.location.href = "cabinet.html";
+} else {
 
-    }else{
+    alert("Невірний номер замовлення або код доступу");
 
-        alert(
-            "Невірний номер замовлення або код доступу"
-        );
-
-    }
+}
+```
 
 }
